8acd488182984562c700ad4dc27648b1b9ef1f05
Shin <542782758@qq.com>
2018-05-15 19:33:13 +0800

    fix report for hw2

--- mnist_project/data_loader.py
+++ mnist_project/data_loader.py
@@ -4,17 +4,20 @@
 import numpy as np
 from PIL import Image
 
-class DataLoader(object):
+class MnistLoader(object):
     DATA_NUM = (60000, 10000)       # The number of figures
     FIG_W = 45                      # width of each figure
 
-    def __init__(self, path='./mnist/'):
+    def __init__(self, fix=True, path='./data/'):
         '''
         :param path: the path to mnist dataset
         '''
 
         self.data_train, self.label_train = self._load(path + 'mnist_train/mnist_train_', 0)
         self.data_test, self.label_test = self._load(path + 'mnist_test/mnist_test_', 1)
+
+        if fix:
+            self._fix()
 
     def _load(self, path, mode=0):
         '''
@@ -31,7 +34,20 @@ class DataLoader(object):
 
         return data, label
 
+    def _fix(self):
+        # flatten the data
+        self.data_train = self.data_train.reshape(self.data_train.shape[0], self.FIG_W * self.FIG_W)
+        self.data_test = self.data_test.reshape(self.data_test.shape[0], self.FIG_W * self.FIG_W)
+
+        # change label into one-hot vector
+        self.label_train = np.eye(10)[self.label_train.reshape(-1)]
+        self.label_test = np.eye(10)[self.label_test.reshape(-1)]
+
     def demo(self):
+        # show the structure of data & label
+        print(self.data_train.shape)
+        print(self.label_train.shape)
+
         # choose a random index
         ind = np.random.randint(0, self.DATA_NUM[0])
 
@@ -40,6 +56,10 @@ class DataLoader(object):
         print("label: ", self.label_train[ind])
 
         # save the figure
-        im = Image.fromarray(self.data_train[ind])
+        im = Image.fromarray(self.data_train[ind].reshape(self.FIG_W, self.FIG_W))
         im.show()
         im.save("demo.png")
+
+if __name__ == '__main__':
+    loader = MnistLoader()
+    loader.demo()
