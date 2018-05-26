15f4fd5d853e570809e2797b1c5c9432725c4640
Shin <542782758@qq.com>
2018-05-26 19:30:04 +0800

    ploting all the way....

--- hw3/normal_part.py
+++ hw3/normal_part.py
@@ -11,11 +11,11 @@ interval = 15
 max_iter = 150
 
 def pca(x):
-    model = PCA(n_components=27)
+    model = PCA(n_components=9)
     data = model.fit_transform(x)
     print(model.explained_variance_ratio_.sum())
-    #return data
-    return x
+    return data
+    #return x
 
 def select(x, y):
     selector = SelectKBest(score_func=f_classif, k=10)
@@ -27,9 +27,9 @@ def select(x, y):
 ################################### SVM Part #####################################
 
 def svm(data_name):
-    x_tr = np.load('data/' + data_name + '_feature.npy')
+    x_tr = pca(np.load('data/' + data_name + '_feature.npy'))
     y_tr = np.load('data/' + data_name + '_target.npy')
-    x_t = np.load('data/' + data_name + '.t_feature.npy')
+    x_t = pca(np.load('data/' + data_name + '.t_feature.npy'))
     y_t = np.load('data/' + data_name + '.t_target.npy')
     if data_name == 'madelon':
         scaler = StandardScaler()
@@ -40,11 +40,12 @@ def svm(data_name):
 
     res_tr = []
     res_t = []
+
     for i in range(interval, max_iter + 1, interval):
-        model = SVC(C=0.6, cache_size=200, class_weight=None, coef0=0.5,
-                decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-                max_iter=i, probability=False, random_state=None, shrinking=True,
-                tol=0.001, verbose=False)
+        model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+                decision_function_shape='ovr', degree=3, gamma='auto', kernel='linear',
+                max_iter=i, probability=False, shrinking=True,
+                tol=0.001, verbose=False, random_state=666)
         model.fit(x_tr, y_tr)
 
         res_tr.append(round(model.score(x_tr, y_tr), 3))
@@ -54,6 +55,208 @@ def svm(data_name):
         # print(model.predict(x_tr))
     print('train: ', res_tr)
     print('test: ', res_t)
+
+def plot_s_kernel_splice():
+    # model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #             decision_function_shape='ovr', degree=3, gamma='auto', kernel='linear',
+    #             max_iter=i, probability=False, shrinking=True,
+    #             tol=0.001, verbose=False, random_state=666)
+    x = list(range(interval, max_iter + 1, interval))
+    y_tr_linear = [0.612, 0.598, 0.633, 0.547, 0.577, 0.599, 0.645, 0.558, 0.589, 0.607]
+    y_tr_poly3 = [0.579, 0.67, 0.718, 0.725, 0.739, 0.767, 0.798, 0.834, 0.864, 0.848]
+    y_tr_rbf = [0.659, 0.719, 0.821, 0.865, 0.894, 0.937, 0.952, 0.981, 0.989, 0.998]
+    y_tr_sigmoid = [0.498, 0.489, 0.488, 0.488, 0.491, 0.491, 0.492, 0.492, 0.492, 0.491]
+    y_t_linear = [0.63, 0.583, 0.65, 0.556, 0.575, 0.589, 0.656, 0.558, 0.62, 0.594]
+    y_t_poly3 = [0.559, 0.694, 0.69, 0.695, 0.733, 0.73, 0.732, 0.781, 0.798, 0.793]
+    y_t_rbf = [0.64, 0.7, 0.774, 0.783, 0.813, 0.844, 0.834, 0.869, 0.874, 0.887]
+    y_t_sigmoid = [0.475, 0.456, 0.453, 0.455, 0.456, 0.456, 0.457, 0.457, 0.457, 0.459]
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_tr_linear, color='#90EE90', linewidth=1.7, label='no kernel')
+    ax.plot(x, y_tr_poly3, color='#ffa07a', linewidth=1.7, label='3-polynomial')
+    ax.plot(x, y_tr_rbf, color='#9999ff', linewidth=1.7, label='rbf')
+    ax.plot(x, y_tr_sigmoid, color='#F0E68C', linewidth=1.7, label='sigmoid')
+    ax.scatter(x, y_tr_linear, s=13, c='#90EE90')
+    ax.scatter(x, y_tr_poly3, s=13, c='#ffa07a')
+    ax.scatter(x, y_tr_rbf, s=13, c='#9999ff')
+    ax.scatter(x, y_tr_sigmoid, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_kernel_splice_tr')
+    plt.show()
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_t_linear, color='#90EE90', linewidth=1.7, label='no kernel')
+    ax.plot(x, y_t_poly3, color='#ffa07a', linewidth=1.7, label='3-polynomial')
+    ax.plot(x, y_t_rbf, color='#9999ff', linewidth=1.7, label='rbf')
+    ax.plot(x, y_t_sigmoid, color='#F0E68C', linewidth=1.7, label='sigmoid')
+    ax.scatter(x, y_t_linear, s=13, c='#90EE90')
+    ax.scatter(x, y_t_poly3, s=13, c='#ffa07a')
+    ax.scatter(x, y_t_rbf, s=13, c='#9999ff')
+    ax.scatter(x, y_t_sigmoid, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_kernel_splice_t')
+    plt.show()
+
+def plot_s_kernel_sat():
+    # model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #             decision_function_shape='ovr', degree=3, gamma='auto', kernel='linear',
+    #             max_iter=i, probability=False, shrinking=True,
+    #             tol=0.001, verbose=False, random_state=666)
+    x = list(range(interval, max_iter + 1, interval))
+    y_tr_linear = [0.633, 0.726, 0.801, 0.655, 0.665, 0.808, 0.833, 0.826, 0.853, 0.857]
+    y_tr_poly3 = [0.642, 0.664, 0.658, 0.638, 0.651, 0.661, 0.664, 0.693, 0.68, 0.644]
+    y_tr_rbf = [0.688, 0.685, 0.676, 0.715, 0.582, 0.624, 0.783, 0.823, 0.826, 0.828]
+    y_tr_sigmoid = [0.657, 0.706, 0.647, 0.681, 0.679, 0.648, 0.683, 0.776, 0.78, 0.798]
+    y_t_linear = [0.56, 0.673, 0.776, 0.662, 0.684, 0.78, 0.796, 0.796, 0.806, 0.825]
+    y_t_poly3 = [0.632, 0.656, 0.652, 0.644, 0.652, 0.66, 0.666, 0.688, 0.674, 0.657]
+    y_t_rbf = [0.664, 0.682, 0.656, 0.707, 0.608, 0.63, 0.764, 0.795, 0.798, 0.804]
+    y_t_sigmoid = [0.659, 0.696, 0.646, 0.674, 0.688, 0.666, 0.688, 0.749, 0.748, 0.764]
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_tr_linear, color='#90EE90', linewidth=1.7, label='no kernel')
+    ax.plot(x, y_tr_poly3, color='#ffa07a', linewidth=1.7, label='3-polynomial')
+    ax.plot(x, y_tr_rbf, color='#9999ff', linewidth=1.7, label='rbf')
+    ax.plot(x, y_tr_sigmoid, color='#F0E68C', linewidth=1.7, label='sigmoid')
+    ax.scatter(x, y_tr_linear, s=13, c='#90EE90')
+    ax.scatter(x, y_tr_poly3, s=13, c='#ffa07a')
+    ax.scatter(x, y_tr_rbf, s=13, c='#9999ff')
+    ax.scatter(x, y_tr_sigmoid, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_kernel_sat_tr')
+    plt.show()
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_t_linear, color='#90EE90', linewidth=1.7, label='no kernel')
+    ax.plot(x, y_t_poly3, color='#ffa07a', linewidth=1.7, label='3-polynomial')
+    ax.plot(x, y_t_rbf, color='#9999ff', linewidth=1.7, label='rbf')
+    ax.plot(x, y_t_sigmoid, color='#F0E68C', linewidth=1.7, label='sigmoid')
+    ax.scatter(x, y_t_linear, s=13, c='#90EE90')
+    ax.scatter(x, y_t_poly3, s=13, c='#ffa07a')
+    ax.scatter(x, y_t_rbf, s=13, c='#9999ff')
+    ax.scatter(x, y_t_sigmoid, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_kernel_sat_t')
+    plt.show()
+
+# poly kernel results: kernel_num (2-10)
+#     splice: tr:[0.852, 0.947, 0.983, 0.99, 0.99, 0.993, 0.99, 0.989, 0.983]
+#             t:[0.786, 0.857, 0.865, 0.871, 0.88, 0.874, 0.864, 0.867, 0.851]
+#     sat: tr:[0.651, 0.672, 0.492, 0.553, 0.475, 0.511, 0.471, 0.524, 0.373]
+#          t:[0.635, 0.666, 0.505, 0.554, 0.488, 0.512, 0.477, 0.528, 0.403]
+
+def plot_s_dim_splice():
+    # model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #             decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    #             max_iter=i, probability=False, shrinking=True,
+    #             tol=0.001, verbose=False, random_state=666)
+    x = list(range(interval, max_iter + 1, interval))
+    y_25_tr = [0.615, 0.75, 0.749, 0.831, 0.853, 0.855, 0.891, 0.942, 0.965, 0.97]
+    y_5_tr = [0.754, 0.762, 0.798, 0.812, 0.867, 0.889, 0.921, 0.966, 0.969, 0.997]
+    y_75_tr = [0.67, 0.7, 0.816, 0.846, 0.874, 0.922, 0.941, 0.966, 0.989, 0.997]
+    y_tr = [0.659, 0.719, 0.821, 0.865, 0.894, 0.937, 0.952, 0.981, 0.989, 0.998]
+    y_25_t = [0.635, 0.571, 0.567, 0.556, 0.585, 0.569, 0.569, 0.573, 0.603, 0.601]
+    y_5_t = [0.591, 0.547, 0.56, 0.585, 0.586, 0.587, 0.587, 0.598, 0.61, 0.585]
+    y_75_t = [0.51, 0.55, 0.584, 0.55, 0.581, 0.645, 0.583, 0.612, 0.637, 0.626]
+    y_t = [0.64, 0.7, 0.774, 0.783, 0.813, 0.844, 0.834, 0.869, 0.874, 0.887]
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_25_tr, color='#90EE90', linewidth=1.7, label='25% features')
+    ax.plot(x, y_5_tr, color='#ffa07a', linewidth=1.7, label='50% features')
+    ax.plot(x, y_75_tr, color='#9999ff', linewidth=1.7, label='75% features')
+    ax.plot(x, y_tr, color='#F0E68C', linewidth=1.7, label='100% features')
+    ax.scatter(x, y_25_tr, s=13, c='#90EE90')
+    ax.scatter(x, y_5_tr, s=13, c='#ffa07a')
+    ax.scatter(x, y_75_tr, s=13, c='#9999ff')
+    ax.scatter(x, y_tr, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_dim_splice_tr')
+    plt.show()
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_25_t, color='#90EE90', linewidth=1.7, label='25% features')
+    ax.plot(x, y_5_t, color='#ffa07a', linewidth=1.7, label='50% features')
+    ax.plot(x, y_75_t, color='#9999ff', linewidth=1.7, label='75% features')
+    ax.plot(x, y_t, color='#F0E68C', linewidth=1.7, label='100% features')
+    ax.scatter(x, y_25_t, s=13, c='#90EE90')
+    ax.scatter(x, y_5_t, s=13, c='#ffa07a')
+    ax.scatter(x, y_75_t, s=13, c='#9999ff')
+    ax.scatter(x, y_t, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_dim_splice_t')
+    plt.show()
+
+def plot_s_dim_sat():
+    # model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+    #             decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
+    #             max_iter=i, probability=False, shrinking=True,
+    #             tol=0.001, verbose=False, random_state=666)
+    x = list(range(interval, max_iter + 1, interval))
+    y_25_tr = [0.617, 0.534, 0.558, 0.732, 0.674, 0.766, 0.764, 0.773, 0.797, 0.806]
+    y_5_tr = [0.661, 0.75, 0.749, 0.704, 0.736, 0.819, 0.802, 0.725, 0.809, 0.846]
+    y_75_tr = [0.709, 0.576, 0.651, 0.803, 0.733, 0.825, 0.811, 0.84, 0.859, 0.835]
+    y_tr = [0.633, 0.726, 0.801, 0.655, 0.665, 0.808, 0.833, 0.826, 0.853, 0.857]
+    y_25_t = [0.489, 0.502, 0.453, 0.496, 0.554, 0.56, 0.536, 0.652, 0.668, 0.663]
+    y_5_t = [0.585, 0.616, 0.515, 0.571, 0.558, 0.648, 0.622, 0.59, 0.632, 0.658]
+    y_75_t = [0.564, 0.426, 0.442, 0.619, 0.487, 0.648, 0.62, 0.648, 0.659, 0.655]
+    y_t = [0.56, 0.673, 0.776, 0.662, 0.684, 0.78, 0.796, 0.796, 0.806, 0.825]
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_25_tr, color='#90EE90', linewidth=1.7, label='25% features')
+    ax.plot(x, y_5_tr, color='#ffa07a', linewidth=1.7, label='50% features')
+    ax.plot(x, y_75_tr, color='#9999ff', linewidth=1.7, label='75% features')
+    ax.plot(x, y_tr, color='#F0E68C', linewidth=1.7, label='100% features')
+    ax.scatter(x, y_25_tr, s=13, c='#90EE90')
+    ax.scatter(x, y_5_tr, s=13, c='#ffa07a')
+    ax.scatter(x, y_75_tr, s=13, c='#9999ff')
+    ax.scatter(x, y_tr, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_dim_sat_tr')
+    plt.show()
+
+    plt.figure(figsize=(6, 4))
+    ax = plt.gca()
+    ax.plot(x, y_25_t, color='#90EE90', linewidth=1.7, label='25% features')
+    ax.plot(x, y_5_t, color='#ffa07a', linewidth=1.7, label='50% features')
+    ax.plot(x, y_75_t, color='#9999ff', linewidth=1.7, label='75% features')
+    ax.plot(x, y_t, color='#F0E68C', linewidth=1.7, label='100% features')
+    ax.scatter(x, y_25_t, s=13, c='#90EE90')
+    ax.scatter(x, y_5_t, s=13, c='#ffa07a')
+    ax.scatter(x, y_75_t, s=13, c='#9999ff')
+    ax.scatter(x, y_t, s=13, c='#F0E68C')
+    ax.grid(color='b', alpha=0.5, linestyle='dashed', linewidth=0.5)
+    plt.xlabel('Iterations')
+    plt.ylabel('Accuracy')
+    plt.legend()
+    plt.savefig('report/img/svm_dim_sat_t')
+    plt.show()
 
 ################################### SVM Part #####################################
 
@@ -539,6 +742,13 @@ def plot_archi_splice():
 
 if __name__ == '__main__':
     # splice satimage.scale
+
+    ## MLP ##
     #mlp('satimage.scale')
     #mlp('splice')
-    plot_archi_splice()
+    #plot_archi_splice()
+
+    ## SVM ##
+    #svm('satimage.scale')
+    #svm('splice')
+    #plot_s_dim_sat()
