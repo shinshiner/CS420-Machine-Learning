2637325110e5ad626434cfee3e2306f5c618cdd2
Shin <542782758@qq.com>
2018-05-30 11:25:36 +0800

    change dataset for hw3 bonus part

--- hw3/bonus_part.py
+++ hw3/bonus_part.py
@@ -1,30 +1,58 @@
 from sklearn.svm import SVC
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
 import numpy as np
+import pickle
+import time
 
-interval = 50
-max_iter = 300
+interval = 100
+max_iter = 100
+
+def cifar_sample():
+    with open('data/cifar-10-batches-py/test_batch', 'rb') as f:
+        dic = pickle.load(f, encoding='bytes')
+        labels = np.array(dic[b'labels'])
+        print(dic[b'data'].shape)
+        np.save('data/cifar-10-batches-py/cifar10-data.t.npy', dic[b'data'])
+        np.save('data/cifar-10-batches-py/cifar10-labels.t.npy', labels)
+
+def merge_batches():
+    labels = []
+    data = np.zeros((50000, 3072))
+    for i in range(1, 6):
+        with open('data/cifar-10-batches-py/data_batch_%d' % i, 'rb') as f:
+            dic = pickle.load(f, encoding='bytes')
+            labels += dic[b'labels']
+            data[10000*(i-1):10000*i] = dic[b'data']
+
+    labels = np.array(labels)
+    #print(labels.shape, data.shape)
+    np.save('data/cifar-10-batches-py/cifar10-data.npy', data)
+    np.save('data/cifar-10-batches-py/cifar10-labels.npy', labels)
+
 
 def svm_bonus():
-    x_tr = np.load('data/convex/convex_train.amat_feature.npy')
-    y_tr = np.load('data/convex/convex_train.amat_target.npy')
-    x_t = np.load('data/convex/convex_test.amat_feature.npy')[27000:33000]
-    y_t = np.load('data/convex/convex_test.amat_target.npy')[27000:33000]
+    x_tr = np.load('data/cifar-10-batches-py/cifar10-data.npy')[:10000]
+    y_tr = np.load('data/cifar-10-batches-py/cifar10-labels.npy')[:10000]
+    x_t = np.load('data/cifar-10-batches-py/cifar10-data.t.npy')
+    y_t = np.load('data/cifar-10-batches-py/cifar10-labels.t.npy')
 
-    scaler = StandardScaler()
-    scaler.fit(x_tr)
-    x_tr = scaler.transform(x_tr)
-    scaler.fit(x_t)
-    x_t = scaler.transform(x_t)
+    # scaler = StandardScaler()
+    # scaler.fit(x_tr)
+    # x_tr = scaler.transform(x_tr)
+    # scaler.fit(x_t)
+    # x_t = scaler.transform(x_t)
 
     res_tr = []
     res_t = []
+    print('start to training')
+    t = time.time()
     for i in range(interval, max_iter + 1, interval):
-        model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=1.0,
-                    decision_function_shape='ovr', degree=5, gamma='auto', kernel='poly',
+        model = SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
+                    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
                     max_iter=i, probability=False, random_state=None, shrinking=True,
                     tol=0.001, verbose=False)
         model.fit(x_tr, y_tr)
+        print('finish training, spending %.4f seconds' % (time.time() - t))
 
         res_tr.append(round(model.score(x_tr, y_tr), 3))
         res_t.append(round(model.score(x_t, y_t), 3))
@@ -35,3 +63,4 @@ def svm_bonus():
 
 if __name__ == '__main__':
     svm_bonus()
+    #cifar_sample()
